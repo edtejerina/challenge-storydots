f093c930cc0f8f576cd5bfc97edaee3babc18377
Edgar Tejerina <edtejerina.work@gmail.com>
2020-12-22 12:32:53 -0300

    error index

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //Creando el servidor
 const app = express();
@@ -14,9 +17,6 @@ app.use(cors());
 
 //Definiendo las rutas
 app.use('/api/weather/', weatherRoute);
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'frontend/build/index.html'));
-  });
 
 //Puerto
 const port = process.env.PORT || 4000;
