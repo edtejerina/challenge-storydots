4e45ebaa13bac730589a1a2b4bde4703accad959
Edgar Tejerina <edtejerina.work@gmail.com>
2020-12-22 12:37:35 -0300

    error

---
+++ build/precache-manifest.e04057b16ca97236df0499825a4bd5d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc4ff1099305429e9637f21043ed42d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "424e5f52405eb1c731be",
+    "url": "/static/css/main.ca3f86c0.chunk.css"
+  },
+  {
+    "revision": "54e58f23e3d81634e956",
+    "url": "/static/js/2.cc06e991.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/static/js/2.cc06e991.chunk.js.LICENSE"
+  },
+  {
+    "revision": "424e5f52405eb1c731be",
+    "url": "/static/js/main.7da6f0dd.chunk.js"
+  },
+  {
+    "revision": "6d89d30b1881c83805d9",
+    "url": "/static/js/runtime-main.e5db6dea.js"
+  }
+]);
